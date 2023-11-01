f5660ca0bcd25ed300f72dd4a53f3474f984dfee
tbutani <tbutani@NO-002-QA.nowonline.server>
2023-11-01 13:04:18 +0530

    Update script-UlogoProfilestatisticsPage

---
+++ YounitedOne/src/main/java/com/younited/qa/pages/UlogoProfilestatisticsPage.java
@@ -0,0 +1,61 @@
+package com.younited.qa.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.younited.qa.base.TestBase;
+
+public class UlogoProfilestatisticsPage extends TestBase{
+	//Page factory OR Object Repository
+	@FindBy(id="initial-sort-select")
+	WebElement roleDropDown;
+	
+	@FindBy(xpath="//*[@id=\"initial-sort-select\"]/option[2]")
+	WebElement selectRole;
+	
+	@FindBy(xpath="//*[@id=\"initial-sort-select\"]/option[1]")
+	WebElement resetRoleDropDown;
+	
+	@FindBy(id="filter-input")
+	WebElement searchField;
+	
+	@FindBy(xpath="//*[@id=\"my-companies\"]/div/div/div/div/div/div[1]/div[2]/div/div/button")
+	WebElement resetBtn;
+	
+	//Initialization
+	public UlogoProfilestatisticsPage() {
+		PageFactory.initElements(Driver, this);
+	}
+	
+	//Actions
+	public void clickRoleDropDown() {
+		roleDropDown.click();
+	}
+	
+	public void selectRole() {
+		selectRole.click();
+	}
+	
+	public void resetRole() {
+		resetRoleDropDown.click();
+	}
+	
+	public void clickSearchField() {
+		searchField.click();
+	}
+	
+	public void enterValueSearchField(String value) {
+		searchField.sendKeys(value);
+	}
+	
+	public void clickResetBtn() {
+		resetBtn.click();
+	}
+	
+	
+	
+
+}
