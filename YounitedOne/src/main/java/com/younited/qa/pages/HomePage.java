de351b905fc190953d81afd496b35af582ae1704
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-02-06 18:57:50 +0530

    Update script-HomePage

--- YounitedOne/src/main/java/com/younited/qa/pages/HomePage.java
+++ YounitedOne/src/main/java/com/younited/qa/pages/HomePage.java
@@ -14,7 +14,7 @@ public class HomePage extends TestBase{
 	@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/a/img[1]")
 	WebElement imageLogo;
 		
-	@FindBy(xpath="//a[@href='/mijn-opdrachten']")
+	@FindBy(xpath="//*[@id=\"navbarSupportedContent\"]/ul[1]/li[3]/a")
 	WebElement myAssignmentsLink;
 		
 	@FindBy(xpath="//*[@id=\"navbarSupportedContent\"]/ul/li[1]/a")
