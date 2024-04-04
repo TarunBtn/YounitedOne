d1166fe27f4bcd6cf65bd03d9ef3af89806cc080
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-04-04 18:02:07 +0530

    UlogoDassAssignmentsPageTest

--- YounitedOne/src/test/java/com/younited/qa/testcases/UlogoDasAssignmentsPageTest.java
+++ YounitedOne/src/test/java/com/younited/qa/testcases/UlogoDasAssignmentsPageTest.java
@@ -29,8 +29,8 @@ public class UlogoDasAssignmentsPageTest extends TestBase{
 		uLogoDasAssignmentsPage=new UlogoDasAssignmentsPage();
 		homePageAdmin=loginPage.loginHomePageAdmin(prop.getProperty("usernametwo"), prop.getProperty("passwordtwo"));
 		testUtil.testWaitFourteen();
-		homePageAdmin.clickAcceptAllCookies();
-		testUtil.testWaitEleven();
+		//homePageAdmin.clickAcceptAllCookies();
+		//testUtil.testWaitEleven();
 		homePageAdmin.clickUserLogo();
 		testUtil.testWaitTwo();
 		homePageAdmin.clickDasAssignmentOverviewTab();
--- YounitedOne/src/test/java/com/younited/qa/testcases/UlogoDasPlatformPageTest.java
+++ YounitedOne/src/test/java/com/younited/qa/testcases/UlogoDasPlatformPageTest.java
@@ -29,8 +29,8 @@ public class UlogoDasPlatformPageTest extends TestBase{
 		testUtil=new TestUtil();
 		homePageAdmin=loginPage.loginHomePageAdmin(prop.getProperty("usernametwo"), prop.getProperty("passwordtwo"));
 		testUtil.testWaitFourteen();
-		homePageAdmin.clickAcceptAllCookies();
-		testUtil.testWaitEleven();
+		//homePageAdmin.clickAcceptAllCookies();
+		//testUtil.testWaitEleven();
 		homePageAdmin.clickUserLogo();
 		testUtil.testWaitFour();
 		homePageAdmin.clickDasPlatformTab();
