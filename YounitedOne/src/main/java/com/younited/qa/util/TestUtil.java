ee13805585507324960e62ca0d2ea815d4216a0e
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-05-30 16:54:33 +0530

    Testutil

--- YounitedOne/src/main/java/com/younited/qa/util/TestUtil.java
+++ YounitedOne/src/main/java/com/younited/qa/util/TestUtil.java
@@ -11,8 +11,8 @@ import com.younited.qa.base.TestBase;
 
 public class TestUtil extends TestBase{
 	
-	public static long PAGE_LOAD_TIMEOUT = 30;
-	public static long IMPLICIT_WAIT = 30;
+	public static long PAGE_LOAD_TIMEOUT = 20;
+	public static long IMPLICIT_WAIT = 20;
 	
 	public void switchToFrameOne() {
 		Driver.switchTo().frame("mainpanel");
