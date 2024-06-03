419c13fda26731b7699d3c30e2ee8ae2b8adf9da
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-06-03 14:33:22 +0530

    HomePageAdmin

--- YounitedOne/src/main/java/com/younited/qa/pages/HomePageAdmin.java
+++ YounitedOne/src/main/java/com/younited/qa/pages/HomePageAdmin.java
@@ -10,193 +10,193 @@ import com.younited.qa.base.TestBase;
 
 public class HomePageAdmin extends TestBase{
 	//Page factory OR Object repository
-	@FindBy(xpath="//span[@class='d-flex']")
-	WebElement assignmentsTab;
+			@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div/div[2]/ul[1]/li[2]/a/span")
+			WebElement assignmentsTab;
+						
+			@FindBy(xpath="//a[normalize-space()='Community']")
+			WebElement communityTab;
+						
+			@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div/div[2]/ul[1]/li[4]/a")
+			WebElement matchTab;
+						
+			@FindBy(xpath="/html/body/div[3]/div[2]/div/div[2]/div[2]/div[2]/div[1]/button/span")
+			WebElement acceptAllCookies;
+					
+			@FindBy(xpath="//img[@class='profile-img']")
+			WebElement userLogo;
+					
+			@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div/div[1]/ul/li[4]/div/div/div/div[2]/ul/span[1]/li[1]/a")
+			WebElement myDashboard;
+					
+			@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div[3]/a")
+			WebElement openMenu;
+					
+			@FindBy(xpath="//*[@id=\"sidebar-right\"]/div/div/button/i")
+			WebElement closeMenu;
+					
+			@FindBy(xpath="//*[@id=\"sidebar-right\"]/div/div/div/a[2]")
+			WebElement chatTab;
+					
+			@FindBy(xpath="//*[@id=\"modal-chat___BV_modal_body_\"]/button")
+			WebElement chatClose;
+					
+			@FindBy(xpath="//*[@id=\"sidebar-right\"]/div/div/div/a[3]")
+			WebElement myNotificationsTab;
+					
+			@FindBy(xpath="//a[@class='manage-profile-button']")
+			WebElement myProfileTab;
+					
+			@FindBy(xpath="//a[normalize-space()='Mijn opdrachten']")
+			WebElement myAssignmentsTab;
+					
+			@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div/div[1]/ul/li[4]/div/button/img")
+			WebElement logoImage;
+					
+			@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div/div[1]/ul/li[4]/div/div/div/div[1]/img")
+			WebElement userLogoPhoto;
+					
+			@FindBy(xpath="//a[normalize-space()='Mijn bedrijf/bedrijven']")
+			WebElement myCompanyTab;
+					
+			@FindBy(xpath="//a[normalize-space()='Mijn reacties']")
+			WebElement myReactionsTab;
+					
+			@FindBy(xpath="//a[normalize-space()='Gebruikers']")
+			WebElement userManagementTab;
+					
+			@FindBy(xpath="//a[normalize-space()='Bedrijven']")
+			WebElement allCompanies;
+					
+			@FindBy(xpath="//a[@href='/netive-bedrijven']")
+			WebElement nativeCompany;
+					
+			@FindBy(xpath="//a[normalize-space()='Opdrachten']")
+			WebElement allAssignments;
+					
+			@FindBy(xpath="//a[normalize-space()='Profielstatistieken']")
+			WebElement profileStatistics;
+					
+			@FindBy(xpath="//a[normalize-space()='Netive Opdrachten overzicht']")
+			WebElement nativeAssignmentsTab;
+			
 				
-	@FindBy(xpath="//a[normalize-space()='Community']")
-	WebElement communityTab;
+			//Initialization
+			public HomePageAdmin() {
+				PageFactory.initElements(Driver, this);
+			}
 				
-	@FindBy(xpath="//a[normalize-space()='Matchen']")
-	WebElement matchTab;
+			//Actions
+			public AssignmentsPageAdmin clickAssignmentsTab() {
+				assignmentsTab.click();		
+				return new AssignmentsPageAdmin();
+			}
 				
-	@FindBy(xpath="/html/body/div[3]/div[2]/div/div[2]/div[2]/div[2]/div[1]/button/span")
-	WebElement acceptAllCookies;
+			public CommunityPageAdmin clickCommunityPageAdmin() {
+				communityTab.click();		
+				return new CommunityPageAdmin();
+			}
+				
+			public MatchPageAdmin clickMatchTab() {
+				matchTab.click();			
+				return new MatchPageAdmin();
+			}
+				
+			public void clickAcceptAllCookies() {
+				acceptAllCookies.click();
+			}
 			
-	@FindBy(xpath="//img[@class='profile-img']")
-	WebElement userLogo;
+			public void clickUserLogo() {
+				userLogo.click();
+			}
 			
-	@FindBy(xpath="//a[normalize-space()='Mijn dashboard']")
-	WebElement myDashboard;
+			public void clickOpenMenu() {
+				openMenu.click();
+			}
 			
-	@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div[3]/a")
-	WebElement openMenu;
+			public void clickCloseMenu() {
+				closeMenu.click();
+			}
 			
-	@FindBy(xpath="//*[@id=\"sidebar-right\"]/div/div/button/i")
-	WebElement closeMenu;
+			public void clickMydashboardTab() {
+				myDashboard.click();
+			}
 			
-	@FindBy(xpath="//*[@id=\"sidebar-right\"]/div/div/div/a[2]")
-	WebElement chatTab;
+			public void clickChatTab() {
+				chatTab.click();
+			}
 			
-	@FindBy(xpath="//*[@id=\"modal-chat___BV_modal_body_\"]/button")
-	WebElement chatClose;
+			public void clickChatCloseSign() {
+				chatClose.click();
+			}
 			
-	@FindBy(xpath="//*[@id=\"sidebar-right\"]/div/div/div/a[3]")
-	WebElement myNotificationsTab;
+			public void clickMynotificationsTab() {
+				myNotificationsTab.click();
+			}
 			
-	@FindBy(xpath="//a[@class='manage-profile-button']")
-	WebElement myProfileTab;
+			public void clickMyProfileTab() {
+				myProfileTab.click();
+			}
 			
-	@FindBy(xpath="//a[normalize-space()='Mijn opdrachten']")
-	WebElement myAssignmentsTab;
+			public void clickMyAssignmentsTab() {
+				myAssignmentsTab.click();
+			}
 			
-	@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div/div[1]/ul/li[4]/div/button/img")
-	WebElement logoImage;
+			public void moveHoverTologoImage() {
+				Actions builder=new Actions(Driver);
+				builder.moveToElement(logoImage).build().perform();
+			}
 			
-	@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div/div[1]/ul/li[4]/div/div/div/div[1]/img")
-	WebElement userLogoPhoto;
+			public void clickMyCompanyTab() {
+				myCompanyTab.click();
+			}
 			
-	@FindBy(xpath="//a[normalize-space()='Mijn bedrijf/bedrijven']")
-	WebElement myCompanyTab;
+			public void clickMyReactionsTab() {
+				myReactionsTab.click();
+			}
 			
-	@FindBy(xpath="//a[normalize-space()='Mijn reacties']")
-	WebElement myReactionsTab;
+			public void clickUserManagementTab() {
+				userManagementTab.click();
+			}
 			
-	@FindBy(xpath="//a[normalize-space()='Gebruikersbeheer']")
-	WebElement userManagementTab;
+			public void clickAllCompaniesTab() {
+				allCompanies.click();			
+			}
 			
-	@FindBy(xpath="//a[normalize-space()='Alle bedrijven']")
-	WebElement allCompanies;
+			public void clickNativeCompanyTab() {
+				//nativeCompany.click();
+				Driver.get("https://acc-freelancemarktplaats-mobility.azurewebsites.net/netive-bedrijven");
+			}
 			
-	@FindBy(xpath="//a[@href='/netive-bedrijven']")
-	WebElement nativeCompany;
+			public void clickAllassignmentsTab() {
+				allAssignments.click();
+			}
 			
-	@FindBy(xpath="//a[normalize-space()='Alle opdrachten']")
-	WebElement allAssignments;
+			public void clickProfileStatistics() {
+				profileStatistics.click();
+			}
 			
-	@FindBy(xpath="//a[normalize-space()='Profielstatistieken']")
-	WebElement profileStatistics;
+			public void clickUserLogoPhoto() {
+				userLogoPhoto.click();
+			}
 			
-	@FindBy(xpath="//a[normalize-space()='Netive Opdrachten overzicht']")
-	WebElement nativeAssignmentsTab;
-	
-		
-	//Initialization
-	public HomePageAdmin() {
-		PageFactory.initElements(Driver, this);
-	}
-		
-	//Actions
-	public AssignmentsPageAdmin clickAssignmentsTab() {
-		assignmentsTab.click();		
-		return new AssignmentsPageAdmin();
-	}
-		
-	public CommunityPageAdmin clickCommunityPageAdmin() {
-		communityTab.click();		
-		return new CommunityPageAdmin();
-	}
-		
-	public MatchPageAdmin clickMatchTab() {
-		matchTab.click();			
-		return new MatchPageAdmin();
-	}
-		
-	public void clickAcceptAllCookies() {
-		acceptAllCookies.click();
-	}
-	
-	public void clickUserLogo() {
-		userLogo.click();
-	}
-	
-	public void clickOpenMenu() {
-		openMenu.click();
-	}
-	
-	public void clickCloseMenu() {
-		closeMenu.click();
-	}
-	
-	public void clickMydashboardTab() {
-		myDashboard.click();
-	}
-	
-	public void clickChatTab() {
-		chatTab.click();
-	}
-	
-	public void clickChatCloseSign() {
-		chatClose.click();
-	}
-	
-	public void clickMynotificationsTab() {
-		myNotificationsTab.click();
-	}
-	
-	public void clickMyProfileTab() {
-		myProfileTab.click();
-	}
-	
-	public void clickMyAssignmentsTab() {
-		myAssignmentsTab.click();
-	}
-	
-	public void moveHoverTologoImage() {
-		Actions builder=new Actions(Driver);
-		builder.moveToElement(logoImage).build().perform();
-	}
-	
-	public void clickMyCompanyTab() {
-		myCompanyTab.click();
-	}
-	
-	public void clickMyReactionsTab() {
-		myReactionsTab.click();
-	}
-	
-	public void clickUserManagementTab() {
-		userManagementTab.click();
-	}
-	
-	public void clickAllCompaniesTab() {
-		allCompanies.click();			
-	}
-	
-	public void clickNativeCompanyTab() {
-		//nativeCompany.click();
-		Driver.get("https://acc-freelancemarktplaats-mobility.azurewebsites.net/netive-bedrijven");
-	}
-	
-	public void clickAllassignmentsTab() {
-		allAssignments.click();
-	}
-	
-	public void clickProfileStatistics() {
-		profileStatistics.click();
-	}
-	
-	public void clickUserLogoPhoto() {
-		userLogoPhoto.click();
-	}
-	
-	public void clickDasAssignmentOverviewTab() {
-		Driver.get("https://acc-nox-freelancemarktplaats-mobility.azurewebsites.net/das-opdrachten/");
-	}
-	
-	public void clickDasPlatformTab() {
-		Driver.get("https://acc-nox-freelancemarktplaats-mobility.azurewebsites.net/das-platform/");
-	}
-	
-	public void clickDasGroupsTab() {
-		Driver.get("https://acc-nox-freelancemarktplaats-mobility.azurewebsites.net/das-groep/");
-	}
-	
-	public void clickDasCompaniesTab() {
-		Driver.get("https://acc-nox-freelancemarktplaats-mobility.azurewebsites.net/das-bedrijven/");
-	}
-	
-	public void clickNativeAssignmentsTab() {
-		nativeAssignmentsTab.click();
-	}
+			public void clickDasAssignmentOverviewTab() {
+				Driver.get("https://acc-nox-freelancemarktplaats-mobility.azurewebsites.net/das-opdrachten/");
+			}
+			
+			public void clickDasPlatformTab() {
+				Driver.get("https://acc-nox-freelancemarktplaats-mobility.azurewebsites.net/das-platform/");
+			}
+			
+			public void clickDasGroupsTab() {
+				Driver.get("https://acc-nox-freelancemarktplaats-mobility.azurewebsites.net/das-groep/");
+			}
+			
+			public void clickDasCompaniesTab() {
+				Driver.get("https://acc-nox-freelancemarktplaats-mobility.azurewebsites.net/das-bedrijven/");
+			}
+			
+			public void clickNativeAssignmentsTab() {
+				nativeAssignmentsTab.click();
+			}
 
 }
