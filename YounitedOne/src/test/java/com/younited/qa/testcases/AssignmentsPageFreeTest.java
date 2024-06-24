1f81e454f2ee709307399ea21a71a6ced19b1dfe
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-06-24 14:43:53 +0530

    AssignmentsPageFreeTest

--- YounitedOne/src/test/java/com/younited/qa/testcases/AssignmentsPageFreeTest.java
+++ YounitedOne/src/test/java/com/younited/qa/testcases/AssignmentsPageFreeTest.java
@@ -29,23 +29,13 @@ public class AssignmentsPageFreeTest extends TestBase{
 		assignmentsPageFree=new AssignmentsPageFree();
 		testUtil=new TestUtil();
 		homePageFree=loginPage.loginHomePageFree(prop.getProperty("usernameone"), prop.getProperty("passwordone"));
-		testUtil.testWaitEleven();
-		//try {
+		testUtil.testWaitEleven();	
 		//homePageFree.clickAcceptAllCookies();
-		//}catch(NoSuchElementException e) {
-			//e.printStackTrace();
-		//}
 		//testUtil.testWaitEleven();
 		homePageFree.clickAssignmentsLink();
 		testUtil.testWaitEight();
 			
 	}
-	
-	//public void assignmentsPageTitleTest()throws Exception {
-		//String title=assignmentsPageFree.assignmentsPageTitle();
-		//Assert.assertEquals(title, "Opdrachten - Younited®");
-		//testUtil.testWaitTwo();
-	//}
 	
 	@Test
 	public void clickFiltersTest()throws Exception {
