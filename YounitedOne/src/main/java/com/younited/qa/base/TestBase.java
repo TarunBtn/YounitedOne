703aa9ccf3103fd9debbf145fa181007cd6cf6ad
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-07-01 11:39:21 +0530

    TestBase

--- YounitedOne/src/main/java/com/younited/qa/base/TestBase.java
+++ YounitedOne/src/main/java/com/younited/qa/base/TestBase.java
@@ -50,15 +50,6 @@ public class TestBase {
 			
 		}
 		
-		//ChromeOptions options = new ChromeOptions();
-         //options.setHeadless(true);
-         //options.addArguments("--headless");
-         //options.addArguments("--no-sandbox");
-         //options.addArguments("--disable-dev-shm-usage");
-            //System.setProperty("webdriver.chrome.driver", "./YounitedAa/src/main/java/chromedriver");
-         //System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
-         //Driver = new ChromeDriver(options);
-		
 		//Driver=new ChromeDriver(options);
 		Driver.manage().window().maximize();
 		Driver.manage().deleteAllCookies();
