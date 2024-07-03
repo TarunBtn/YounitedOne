0dfd77911d75a4314718343f9770f380a3a16d8b
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-07-03 12:05:10 +0530

    DashboardPageFree

---
+++ YounitedOne/src/test/java/com/younited/qa/testcases/DashboardPageFreeTest.java
@@ -0,0 +1,47 @@
+package com.younited.qa.testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.younited.qa.base.TestBase;
+import com.younited.qa.pages.HomePage;
+import com.younited.qa.pages.LoginPage;
+import com.younited.qa.util.TestUtil;
+
+public class DashboardPageFreeTest extends TestBase{
+	
+	LoginPage loginPage;
+	HomePage homePage;
+	TestUtil testUtil;
+	
+	public DashboardPageFreeTest() {
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp()throws Exception {
+		initialization();
+		loginPage=new LoginPage();
+		testUtil=new TestUtil();
+		homePage=loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+			
+	}
+	
+	@Test
+	public void test()throws Exception {
+		System.out.println("Test is passed");
+		testUtil.testWaitTwo();
+		homePage.clickSignOutLink();
+		testUtil.testWaitFour();
+		homePage.clickSignOutBtn();
+		testUtil.testWaitFour();
+		
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		Driver.quit();
+	}
+
+}
