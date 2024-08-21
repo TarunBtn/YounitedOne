59d2634575db3bcb4eca24a223e2e777a90d8344
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-08-21 17:37:42 +0530

    UlogoMyreactionsPageTest

--- YounitedOne/src/test/java/com/younited/qa/testcases/UlogoMyreactionsPageTest.java
+++ YounitedOne/src/test/java/com/younited/qa/testcases/UlogoMyreactionsPageTest.java
@@ -31,7 +31,7 @@ public class UlogoMyreactionsPageTest extends TestBase{
 		//homePageAdmin.clickAcceptAllCookies();
 		//testUtil.testWaitEleven();
 		homePageAdmin.clickUserLogo();
-		testUtil.testWaitEight();
+		testUtil.testWaitEleven();
 		homePageAdmin.clickMyReactionsTab();
 		testUtil.testWaitEight();
 		
