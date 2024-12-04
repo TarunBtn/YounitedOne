5da021d5adfa3cfcdae9800c5e6bcde23f0e2357
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-12-04 20:23:50 +0530

    HomePage

--- YounitedOne/src/main/java/com/younited/qa/pages/HomePage.java
+++ YounitedOne/src/main/java/com/younited/qa/pages/HomePage.java
@@ -7,100 +7,99 @@ import org.openqa.selenium.support.PageFactory;
 import com.younited.qa.base.TestBase;
 
 public class HomePage extends TestBase{
-	//Page factory or Object Repository
-			@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div[2]/a/img")
-			WebElement userNameLabel;
-				
-			@FindBy(xpath="//img[@class='d-none d-lg-block']")
-			WebElement imageLogo;
-			
-			@FindBy(xpath="//div[@class='multiselect__select']")
-			WebElement reactiesDropDown;
-				
-			@FindBy(xpath="//a[@class='nav-link'][normalize-space()='Mijn opdrachten']")
-			WebElement myAssignmentsLink;
-				
-			@FindBy(xpath="//a[@class='nav-link active']")
-			WebElement dashboardLink;
-				
-			@FindBy(xpath="//a[normalize-space()='Matchen']")
-			WebElement matchLink;
-				
-			@FindBy(xpath="/html/body/div[3]/div[2]/div/div[2]/div[2]/div[2]/div[1]/button/span")
-			WebElement acceptAllCookies;
-			
-			@FindBy(xpath="//button[@class='dropdown-toggle dropdown-button profile-img']")
-			WebElement userLogo;
-			
-			@FindBy(xpath="//a[@class='manage-profile-button flex-grow-1']")
-			WebElement manageProfileLink;
-			
-			@FindBy(xpath="//a[normalize-space()='Reacties']")
-			WebElement myReactionsLink;
-			
-			@FindBy(xpath="//a[normalize-space()='Mijn bedrijf / bedrijven']")
-			WebElement myCompanyLink;
-			
-			@FindBy(xpath="//a[@class='user-infoNav-item-link logout']//span[@class='material-icons'][normalize-space()='logout']")
-			WebElement signOutLink;
-			
-			@FindBy(xpath="//button[@class='btn btn-primary btn btn-primary']")
-			WebElement signOutBtn;
-				
-			//Initialization
-			public HomePage() {
-				PageFactory.initElements(Driver, this);
-			}
-				
-			//Actions
-			public String verifyHomePageTitle() {
-				return Driver.getTitle();
-			}
-				
-			public boolean verifyImageLogo() {
-				return imageLogo.isDisplayed();
-			}
-			
-			public void clickReactiesDropDown() {
-				reactiesDropDown.click();
-			}
-				
-			public MatchPage clickMatchLink() {
-				matchLink.click();
-				return new MatchPage();
-			}
-				
-			public void clickMyAssignmentsLink() {
-				myAssignmentsLink.click();
-				//return new MyAssignmentsPage();
-			}
-				
-			public void clickAcceptAllCookies() {
-				acceptAllCookies.click();
-			}
-			
-			public void clickUserLogo() {
-				userLogo.click();
-			}
-			
-			public void clickManageProfileLink() {
-				manageProfileLink.click();
-			}
-			
-			public void clickMyReactionsTab() {
-				myReactionsLink.click();
-			}
-			
-			public void clickMycompanyTab() {
-				myCompanyLink.click();
-			}
-			
-			public void clickSignOutLink() {
-				signOutLink.click();
-			}
-			
-			public void clickSignOutBtn() {
-				signOutBtn.click();
-			}
+	@FindBy(xpath="//*[@id=\"main-nav\"]/nav/div/div[2]/a/img")
+	WebElement userNameLabel;
+		
+	@FindBy(xpath="//img[@class='d-none d-lg-block']")
+	WebElement imageLogo;
+	
+	@FindBy(xpath="//div[@class='multiselect__select']")
+	WebElement reactiesDropDown;
+		
+	@FindBy(xpath="//a[@class='nav-link'][normalize-space()='Mijn opdrachten']")
+	WebElement myAssignmentsLink;
+		
+	@FindBy(xpath="//a[@class='nav-link active']")
+	WebElement dashboardLink;
+		
+	@FindBy(xpath="//a[normalize-space()='Matchen']")
+	WebElement matchLink;
+		
+	@FindBy(xpath="/html/body/div[3]/div[2]/div/div[2]/div[2]/div[2]/div[1]/button/span")
+	WebElement acceptAllCookies;
+	
+	@FindBy(xpath="//button[@class='dropdown-toggle dropdown-button profile-img']")
+	WebElement userLogo;
+	
+	@FindBy(xpath="//a[@class='manage-profile-button flex-grow-1']")
+	WebElement manageProfileLink;
+	
+	@FindBy(xpath="//a[normalize-space()='Reacties']")
+	WebElement myReactionsLink;
+	
+	@FindBy(xpath="//a[normalize-space()='Mijn bedrijf / bedrijven']")
+	WebElement myCompanyLink;
+	
+	@FindBy(xpath="//a[@class='user-infoNav-item-link logout']//span[@class='material-icons'][normalize-space()='logout']")
+	WebElement signOutLink;
+	
+	@FindBy(xpath="//button[@class='btn btn-primary btn btn-primary']")
+	WebElement signOutBtn;
+		
+	//Initialization
+	public HomePage() {
+		PageFactory.initElements(Driver, this);
+	}
+		
+	//Actions
+	public String verifyHomePageTitle() {
+		return Driver.getTitle();
+	}
+		
+	public boolean verifyImageLogo() {
+		return imageLogo.isDisplayed();
+	}
+	
+	public void clickReactiesDropDown() {
+		reactiesDropDown.click();
+	}
+		
+	public MatchPage clickMatchLink() {
+		matchLink.click();
+		return new MatchPage();
+	}
+		
+	public void clickMyAssignmentsLink() {
+		myAssignmentsLink.click();
+		//return new MyAssignmentsPage();
+	}
+		
+	public void clickAcceptAllCookies() {
+		acceptAllCookies.click();
+	}
+	
+	public void clickUserLogo() {
+		userLogo.click();
+	}
+	
+	public void clickManageProfileLink() {
+		manageProfileLink.click();
+	}
+	
+	public void clickMyReactionsTab() {
+		myReactionsLink.click();
+	}
+	
+	public void clickMycompanyTab() {
+		myCompanyLink.click();
+	}
+	
+	public void clickSignOutLink() {
+		signOutLink.click();
+	}
+	
+	public void clickSignOutBtn() {
+		signOutBtn.click();
+	}
 
 }
--- YounitedOne/src/main/java/com/younited/qa/pages/UlogoIntAddNewUserPage.java
+++ YounitedOne/src/main/java/com/younited/qa/pages/UlogoIntAddNewUserPage.java
@@ -9,79 +9,79 @@ import com.younited.qa.base.TestBase;
 
 public class UlogoIntAddNewUserPage extends TestBase{
 	//Pagefactory OR Object Repository
-	@FindBy(xpath="//input[@id='filter-input']")
-	WebElement searchUser;
-	
-	@FindBy(xpath="//input[@id='filter-input']")
-	WebElement cancelSearchUser;
-	
-	@FindBy(xpath="//a[@class='btn btn-primary-outline mr-auto']")
-	WebElement addUser;
-	
-	@FindBy(xpath="//input[@class='simple-typeahead-input']")
-	WebElement emailAddress;
-	
-	@FindBy(xpath="//input[@id='voornaam']")
-	WebElement firstName;
-	
-	@FindBy(xpath="//input[@id='achternaam']")
-	WebElement lastName;
-	
-	@FindBy(xpath="//button[@class='btn btn-primary btn-sm']")
-	WebElement addUserBtn;
-	
-	@FindBy(xpath="//button[@class='btn btn-primary-outline btn-sm']")
-	WebElement cancelBtn;
-	
-	//Initialization
-	public UlogoIntAddNewUserPage() {
-		PageFactory.initElements(Driver, this);
-	}
-	
-	//Actions
-	public void clickSearchUser() {
-		searchUser.click();
-	}
-	
-	public void enterSearchUser(String value) {
-		searchUser.sendKeys(value);
-	}
-	
-	public void cancelSearchUser() {
-		cancelSearchUser.click();
-	}
-	
-	public void clickAddUser() {
-		addUser.click();
-	}
-	
-	public void clickEmailAddress() {
-		emailAddress.click();
-	}
-	
-	public void enterEmailAddress(String value) {
-		emailAddress.sendKeys(value);
-	}
-	
-	public void selectEmailTooltip() {
-		emailAddress.sendKeys(Keys.TAB);
-	}
-	
-	public void enterFirstName(String value) {
-		firstName.sendKeys(value);
-	}
-	
-	public void enterLastName(String value) {
-		lastName.sendKeys(value);
-	}
-	
-	public void addUser() {
-		addUserBtn.click();
-	}
-	
-	public void clickCancelBtn() {
-		cancelBtn.click();
-	}
+			@FindBy(xpath="//input[@id='filter-input']")
+			WebElement searchUser;
+			
+			@FindBy(xpath="//input[@id='filter-input']")
+			WebElement cancelSearchUser;
+			
+			@FindBy(xpath="//a[@class='btn btn-primary-outline mr-auto']")
+			WebElement addUser;
+			
+			@FindBy(xpath="//input[@class='simple-typeahead-input']")
+			WebElement emailAddress;
+			
+			@FindBy(xpath="//input[@id='voornaam']")
+			WebElement firstName;
+			
+			@FindBy(xpath="//input[@id='achternaam']")
+			WebElement lastName;
+			
+			@FindBy(xpath="//button[@class='btn btn-primary btn-sm']")
+			WebElement addUserBtn;
+			
+			@FindBy(xpath="//button[@class='btn btn-primary-outline btn-sm']")
+			WebElement cancelBtn;
+			
+			//Initialization
+			public UlogoIntAddNewUserPage() {
+				PageFactory.initElements(Driver, this);
+			}
+			
+			//Actions
+			public void clickSearchUser() {
+				searchUser.click();
+			}
+			
+			public void enterSearchUser(String value) {
+				searchUser.sendKeys(value);
+			}
+			
+			public void cancelSearchUser() {
+				cancelSearchUser.click();
+			}
+			
+			public void clickAddUser() {
+				addUser.click();
+			}
+			
+			public void clickEmailAddress() {
+				emailAddress.click();
+			}
+			
+			public void enterEmailAddress(String value) {
+				emailAddress.sendKeys(value);
+			}
+			
+			public void selectEmailTooltip() {
+				emailAddress.sendKeys(Keys.TAB);
+			}
+			
+			public void enterFirstName(String value) {
+				firstName.sendKeys(value);
+			}
+			
+			public void enterLastName(String value) {
+				lastName.sendKeys(value);
+			}
+			
+			public void addUser() {
+				addUserBtn.click();
+			}
+			
+			public void clickCancelBtn() {
+				cancelBtn.click();
+			}
 	
 
 }
