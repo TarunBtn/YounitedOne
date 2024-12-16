8ce276f4b5602f7cfab114d04234d268c43d1706
tbutani <tbutani@NO-002-QA.nowonline.server>
2024-12-16 19:54:44 +0530

    LeadsPageAdminTest

--- YounitedOne/src/test/java/com/younited/qa/testcases/LeadsPageAdminTest.java
+++ YounitedOne/src/test/java/com/younited/qa/testcases/LeadsPageAdminTest.java
@@ -1,5 +1,6 @@
 package com.younited.qa.testcases;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -29,6 +30,12 @@ public class LeadsPageAdminTest extends TestBase{
 		testUtil=new TestUtil();
 		homePageAdmin=loginPage.loginHomePageAdmin(prop.getProperty("usernametwo"), prop.getProperty("passwordtwo"));
 		testUtil.testWaitFourteen();
+		try {
+			homePageAdmin.clickAcceptAllCookies();
+		}catch(NoSuchElementException e) {
+			e.printStackTrace();
+		}
+		testUtil.testWaitFour();
 		testUtil.scrollDown();
 		testUtil.testWaitEight();
 		homePageAdmin.clickLeadTab();
